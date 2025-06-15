1b1c4588d61960b173fc1ae99732a88a3baf3544
Ivan Repiso Camara <irepisocamara@gmail.com>
2025-06-15 21:04:57 +0200

    Feat: Add models and dtos

---
+++ src/main/java/es/myprojects/todo/dtos/TaskDto.java
@@ -0,0 +1,20 @@
+package es.myprojects.todo.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TaskDto {
+
+    private Long id;
+    @NotBlank(message = "Title is required")
+    private String title;
+    private String description;
+    private boolean completed;
+}
---
+++ src/main/java/es/myprojects/todo/models/User.java
@@ -0,0 +1,58 @@
+package es.myprojects.todo.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // Por simplicidad, todos los usuarios tienen la misma autoridad "ROLE_USER"
+        // En un proyecto más complejo, tendrías roles y permisos más sofisticados.
+        return Collections.singletonList(() -> "ROLE_USER");
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
